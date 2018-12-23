cfacb1fb37eb9c5ac379d47ee8ada0644f762705
Patrick Lin <patrick.lin@ses.com>
2018-12-23 18:40:22 -0500

    init header footer and sidebar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,13 @@
 import { NgModule, SkipSelf, Optional } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule]
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent, FooterComponent, SidebarComponent]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parent: CoreModule) {
